042f0ac21bd64ba6343fc6c6054422bfbb9f9677
Shivamkr.Bharti@13 <shivambhartirxl@gmail.com>
2026-07-27 23:07:24 +0530

    setup for order page activity

---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,39 @@
+
+
+// Placing Order using COD method
+
+const placeOrder = async(req,res)=>{
+
+}
+
+//placong Order using STRIP
+
+const placeOrderStrip = async(req,res)=>{
+
+}
+
+// placing order using razorpay
+
+const placeOrderRazorpay = async(req,res)=>{
+
+}
+
+//All orders data for admin panel
+
+const allOrders = async(req,res)=>{
+
+}
+
+// user order data for frontend
+
+const userData = async(req,res)=>{
+
+}
+
+// update order status from admin panel
+
+const updateStatus = async(req,res)=>{
+
+}
+
+export {placeOrder , placeOrderStrip , placeOrderRazorpay , allOrders , userData , updateStatus}
